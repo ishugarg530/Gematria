6b35932d25d77d0d1dc432d27340be646327a0b1
visualstudio <visualstudio@122-PC15>
2019-04-12 18:13:45 -0400

    code after debug

--- Assignment5/Program.cs
+++ Assignment5/Program.cs
@@ -7,34 +7,47 @@ using System.Threading.Tasks;
 namespace Assignment5
 {
     class Program2
+
     {
-        
-            string[] Letters = new string[26] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
-            public static void Main()
-            {
+        string[] Letters = new string[26] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+
+        public static void Main()
+        {
+
             Program2 orange = new Program2();
 
-            Console.WriteLine(orange.Gematria("Universe"));
-            }
+            Console.WriteLine(orange.Gematria("universe"));
+
+            Console.ReadLine();
+        }
+
         public int Gematria(string word)
         {
+
             int GematriaValue = 0;
-            for (int i = 0; i< word.Length; i++)
+
+            for (int i = 0; i < word.Length; i++)
+
             {
                 GematriaValue += LetterValue(word[i].ToString());
             }
+
             return GematriaValue;
         }
 
         public int LetterValue(string letter)
         {
+
             int x = 0;
-            while (Letters[x]!= letter)
+
+            while (Letters[x] != letter)
+
             {
+
                 if (Letters[x++] == letter)
-                    return x;
+                    return -1;
             }
-            return -1;
+            return x + 1;
         }
     }
 }
